dc45464e9ae043b9be89340f245c8dc6c223e7ed
Conrado Uraga <bromodachi@Conrados-MacBook-Pro.local>
2016-12-11 20:26:43 +0900

    Initial Commit

---
+++ TableViewDrag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewDrag
+//
+//  Created by Conrado Uraga on 2016/12/11.
+//  Copyright © 2016 Conrado Uraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
